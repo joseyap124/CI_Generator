c09a1d7e7a350d49846dc4689a40d3fe0cbadd47
joseyap124 <joseyap123@gmail.com>
2019-03-11 18:51:43 +0700

    MY_Controller Done

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -24,5 +24,56 @@ class MY_Controller extends CI_controller
 {
     return $this->input->get($name, $clean);
 }
+    protected function METHOD()
+{
+    return $this->input->method();
+}
+    protected function flashmsg($msg, $type = 'success', $name='msg')
+{
+    return $this->session->set_flashdata($name, '<div class="alert font-weight-normal alert-'.$type.' alert-dismissable"> <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'.$msg.'</div>');
+}
+    protected function upload($id, $directory, $tag_name = 'userfile', $max_size = 0)
+{
+    if(isset($_FILES[$tag_name]) && !empty($_FILES[$tag_name]['name']))
+    {
+        $upload_path = realpath(APPPATH . '../assets/img/' . $directory . '/');
+        @unlink($upload_path . '/' . $id . '.jpg');
+        $config = array(
+            'file_name' 		=> $id . '.jpg',
+            'allowed_types'		=> 'jpg|png|bmp|jpeg|',
+            'upload_path'		=> $upload_path,
+            'max_size'          => $max_size
+        );
+        $this->load->library('upload');
+        $this->upload->initialize($config);
+        return $this->upload->do_upload($tag_name);
+    }
+}
+protected function uploadFile($name, $directory, $tag_name = 'userfile')
+{
+    if($_FILES[$tag_name])
+    {
+        $upload_path = realpath(APPPATH . '../assets/file/' . $directory . '/');
+        @unlink($upload_path . '/' . $name);
+        $config = array(
+            'file_name' 		=> $name ,
+            'allowed_types'		=> '*',
+            'upload_path'		=> $upload_path,
+            'max_size'          => $max_size
+        );
+        $this->load->library('upload');
+        $this->upload->initialize($config);
+        return $this->upload->do_upload($tag_name);
+        $eks = explodes(".",$_FILES[$tag_name]['name']);
+        return $name."".$eks[1];
+    }
+    return "false";
+}
+public function dump($var)
+	{
+		echo '<pre>';
+		var_dump($var);
+		echo '</pre>';
+	}
 }
 ?>
